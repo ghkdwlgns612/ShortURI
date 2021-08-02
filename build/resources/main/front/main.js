e7036fe3be772085acc4c686bcfbd433e7e859b3
ghkdwlgns612 <ghkdwlgns612@naver.com>
2021-08-02 15:22:19 +0900

    최종완료

--- build/resources/main/front/main.js
+++ build/resources/main/front/main.js
@@ -1,4 +1,5 @@
 const box = document.getElementById('box');
+const scroll = document.getElementById('scroll');
 
 function reqList() {
     const input = document.getElementById('input').value;
@@ -14,10 +15,10 @@ function reqList() {
 function render(data) {
     if(data.hasOwnProperty('err')){
         let resp = data.err + "는 " + data.message;
-        box.innerHTML += "<p>"+ resp +"</p>";
+        scroll.innerHTML += "<p id='add'>"+ resp +"</p>";
     } else {
         console.log(data);
-        let resp = data.data.changedUri + "변환되었습니다.";
-        box.innerHTML += "<p>"+ resp +"</p>";
+        let resp ="http://localhost:8080/sa/" + data.data.changedUri + "변환되었습니다.";
+        scroll.innerHTML += "<p id='add'>"+ resp +"</p>";
     }
 }
